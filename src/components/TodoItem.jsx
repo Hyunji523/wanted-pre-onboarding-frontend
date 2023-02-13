ef0701b3385de9b65521bd9936a64b0b3d1f41bd
이현지 <hyunzzi@iansit.co.kr>
2023-02-13 16:36:47 +0900

    fix : 수정 모드 로직 변경

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -11,38 +11,31 @@ const TodoItem = ({ todo, id, editTodo, deleteTodo }) => {
     setModify(false);
   }
   
-    return (
+  return (
       <li className="todoapp__item" key={id}>
-        <form onSubmit={modifyInput}>
-        {/* 체크박스 */}
-          <input 
-            type="checkbox"
-            className="todoapp__item-checkbox"
-            checked={todo.isCompleted}
-            onChange={(event) => {
-              editTodo(todo.id, todo.todo, event.target.checked);
-            }}
+        
+        { modify?( //수정모드
+          <form onSubmit={modifyInput}>
+          {/* 수정입력창 */}
+            <input type="text" className='modify-input' value={newTodo}
+              onChange={(event) => setNewTodo(event.target.value)}/>
+            <button className="todoapp__item-edit-btn" type="submit" data-testid="submit-button" variant="contained">제출</button>
+            <button type="button" className="todoapp__item-delete-btn" data-testid="cancel-button" onClick={()=>{setModify(false)}}>취소</button>
+          </form>
+          ):( //보기모드
+            <>
+            {/* 체크박스 */}
+            <input 
+              type="checkbox"
+              className="todoapp__item-checkbox"
+              checked={todo.isCompleted}
+              onChange={(event) => {editTodo(todo.id, todo.todo, event.target.checked);}}
             />
-        {/* 할일 텍스트 / 입력창 */}
-        {
-            !modify ? (<span className={!todo.isCompleted ? "todoapp__item-ctx" : "todoapp__item-ctx_checked"}>{todo.todo}</span>
-            ):(<input type="text" className='modify-input' value={newTodo}
-            onChange={(event) => setNewTodo(event.target.value)}/>)
+            <span className={!todo.isCompleted ? "todoapp__item-ctx" : "todoapp__item-ctx_checked"}>{todo.todo}</span>
+            <button type="button" className="todoapp__item-edit-btn" onClick={() => setModify(true)} data-testid="modify-button">수정</button>
+            <button type="button" className="todoapp__item-delete-btn" data-testid="delete-button" onClick={() => deleteTodo(todo.id)}>삭제</button>
+            </> )
         }
-        {/* 수정 */}
-        {
-            !todo.isCompleted ?(
-                !modify ?(<button type="button" className="todoapp__item-edit-btn" onClick={() => setModify(true)} data-testid="modify-button">수정</button>
-                ):(<button className="todoapp__item-edit-btn" type="submit" data-testid="submit-button" variant="contained">제출</button>)
-            ) : null
-        }
-        {/* 삭제 */}
-        {   
-            !modify ? (<button type="button" className="todoapp__item-delete-btn" data-testid="delete-button" onClick={() => deleteTodo(todo.id)}>삭제</button>
-            ):(<button type="button" className="todoapp__item-delete-btn" data-testid="cancel-button" onClick={()=>{setModify(false)}}>취소</button>)
-        }
-        </form>
       </li>
-    );
-  }
+)}
 export default TodoItem;
